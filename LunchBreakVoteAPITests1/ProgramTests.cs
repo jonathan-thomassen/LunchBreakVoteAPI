1bb4bfb681e556f15cbe086b30a2a73b86060b94
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2021-10-11 10:15:59 +0200

    Test Upd

---
+++ LunchBreakVoteAPITests1/ProgramTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LunchBreakVoteAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchBreakVoteAPI.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateHostBuilderTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
